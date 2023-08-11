d902ca6399b261f7242ba381fba664d99c287b85
kkamara <kamaracomputers@gmail.com>
2023-08-11 21:24:15 +0100

    Update User Agent

--- seleniumpy/management/commands/crawl.py
+++ seleniumpy/management/commands/crawl.py
@@ -5,6 +5,7 @@ import os
 import json
 import time
 import logging
+import random
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -24,7 +25,8 @@ class Command(BaseCommand):
             chrome_options.add_argument('--disable-gpu')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-            user_agent = 'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.1+ (KHTML, like Gecko) Version/10.0.0.1337 Mobile Safari/537.1+'
+            user_agent = self.get_random_user_agent()
+            logging.info(f'Using User Agent : '+user_agent)
             chrome_options.add_argument('--user-agent='+user_agent)
             # Use SELENIUM_HEADLESS in .env to remove GUI.
             if settings.SELENIUM_HEADLESS == True or settings.APP_ENV == 'testing':
@@ -65,3 +67,9 @@ class Command(BaseCommand):
         if el:
             el_text = browser.execute_script('return arguments[0].innerText', el)
             logging.info(el_text)
+
+    def get_random_user_agent(self):
+        user_agents = [
+            'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.1+ (KHTML, like Gecko) Version/10.0.0.1337 Mobile Safari/537.1+', 'Mozilla/5.0 (PlayBook; U; RIM Tablet OS 2.1.0; en-US) AppleWebKit/536.2+ (KHTML, like Gecko) Version/7.2.1.0 Safari/536.2+', 'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+']
+        key = random.randint(0, 2)
+        return user_agents[key]
